336406801ef88d8dfc7622e65486b2430aff422c
Mohamed Akeel <akeelaslamcse@gmail.com>
2025-04-21 23:06:21 +0530

    made some changes 21 a

---
+++ src/WebAPI/Controllers/ApiControllerBase.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class ApiControllerBase : ControllerBase
+    {
+        private ISender _mediator;
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
--- src/WebAPI/Controllers/Deals.cs
+++ src/WebAPI/Controllers/Deals.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
-    public class Deals
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Deals : ControllerBase
     {
-        
+
     }
 }
